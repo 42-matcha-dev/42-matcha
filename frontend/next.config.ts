cca8066e7031c28468a1b48ec7caae20436e0093
odkaz <one.destiny.japan@gmail.com>
2025-10-28 21:53:01 +0100

    connect to supabase

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,15 +4,4 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
-// module.exports = {
-//   async rewrites() {
-//     return [
-//       {
-//         source: '/api/hello',
-//         destination: 'http://localhost:5000/api/hello',
-//       },
-//     ];
-//   },
-// };
-
 export default nextConfig;
