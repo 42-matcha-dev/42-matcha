4ca29bbd2b5fe261f32135d9c5ea39a65981b14c
odkaz <one.destiny.japan@gmail.com>
2025-10-29 18:58:35 +0100

    upload photos to supabase

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import { initDB } from './database/init.js';
-import authRoutes from './routes/auth.routes.ts';
+import authRoutes from './routes/auth.routes.js';
+import uploadRoutes from './routes/upload.routes.js';
 
 const app = express();
 const port = process.env.PORT_BACKEND;
@@ -9,6 +10,7 @@ const port = process.env.PORT_BACKEND;
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api', uploadRoutes);
 app.get('/api/hello', (_, res) => {
   res.json({ message: 'Hello from backend!' });
 });
