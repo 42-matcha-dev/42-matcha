9eca3a0f0398d5f9feb2ca815e4dc7592889ca1f
edesaint <edesaint@student.42.fr>
2025-11-13 15:18:33 +0100

    feat(frontend): update schema.ts

--- frontend/src/app/schema.ts
+++ frontend/src/app/schema.ts
@@ -12,14 +12,6 @@ const longText = (min = 3, max = 150) =>
     .min(min, { message: `Le texte doit contenir au moins ${min} caractères.` })
     .max(max, { message: `Le texte doit contenir au maximum ${max} caractères.` });
 
-<<<<<<< Updated upstream
-=======
-// const imageFile = z
-//   .instanceof(File)
-//   .refine(file => file.size < 5 * 1024 * 1024, "L'image doit faire moins de 5Mo")
-//   .refine(file => ["image/jpeg", "image/png"].includes(file.type), "Format non supporté");
-
->>>>>>> Stashed changes
 export const registerSchema = z.object({
   email: z.email(),
   password: shortText(),
@@ -35,16 +27,8 @@ export const registerSchema = z.object({
     .min(1, { message: "Veuillez sélectionner au moins un tag." })
     .max(5, { message: "Vous pouvez sélectionner au maximum 5 tags." }),
   // terms: z.boolean().refine(data => data, "Vous devez accepter les conditions"),
-<<<<<<< Updated upstream
-  iconImage: z.string().url({ message: "L'URL de l'icône doit être valide." }),
-  photos: z
-    .array(z.string().url({ message: "Les URLs des photos doivent être valides." }))
-    .min(1, "Veuillez uploader au moins une photo.")
-    .max(4, "Vous pouvez uploader jusqu'à 4 photos."),
-=======
   iconUrl: z.string().url().nullable(),
   photoUrls: z.array(z.string().url()).max(4),
->>>>>>> Stashed changes
 });
 
 export type RegisterSchema = z.infer<typeof registerSchema>;
