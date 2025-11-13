4f1d9589ea29e17224cf86926d73ed9e5a289f1b
odkaz <one.destiny.japan@gmail.com>
2025-11-13 18:47:39 +0100

    update header color

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="bg-orange-500 text-white p-4">
+    <header className="bg-primary text-white p-4">
         <h1 className="text-xl text-left font-bold">Matcha</h1>
     </header>
   );
