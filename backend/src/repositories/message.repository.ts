ef7fd446cf469bb14061c46bb07f549cc6b0127e
hosonu <hosonoyuki07@gmail.com>
2026-03-04 11:11:54 +0100

    feat: add types for message rows, and implement insertMessage func to message.repository.ts

--- backend/src/repositories/message.repository.ts
+++ backend/src/repositories/message.repository.ts
@@ -1,5 +1,26 @@
-// import pool from '../database/init.js';
+import pool from '../database/init.js';
+
+type MessageRow = {
+    id: number;
+    conversation_id: number;
+    sender_id: number;
+    content: string;
+    created_at: Date;
+    is_read: boolean;
+};
 
 export const messageRepository = {
-
+    insertMessage: async (
+        conversationId: number,
+        senderId: number,
+        content:string
+    ): Promise<MessageRow | undefined> => {
+        const query = `
+            INSERT INTO messages (conversation_id, sender_id, content)
+            VALUES($1, $2, $3)
+            RETURNING id, conversation_id, sender_id, content, created_at, is_read
+        `;
+        const res = await pool.query(query, [conversationId, senderId, content]);
+        return res.rows[0];
+    },
 };
