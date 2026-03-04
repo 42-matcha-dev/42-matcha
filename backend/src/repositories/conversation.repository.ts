de4afff209b23dc04552a989540e04f88fb579e7
hosonu <hosonoyuki07@gmail.com>
2026-03-04 11:34:41 +0100

    feat: add new type for Conversation Row, add id to RETURNING

--- backend/src/repositories/conversation.repository.ts
+++ backend/src/repositories/conversation.repository.ts
@@ -1,13 +1,20 @@
 import pool from '../database/init.js';
 
+type ConversationRow = {
+  id: number;
+  user1_id: number;
+  user2_id: number;
+  created_at: Date;
+};
+
 export const conversationRepository = {
-  createConversation: async (user1_id: number, user2_id: number) => {
+  createConversation: async (user1_id: number, user2_id: number): Promise<ConversationRow | undefined> => {
     const query = `
       INSERT INTO conversations (user1_id, user2_id)
       VALUES ($1, $2)
       ON CONFLICT (user1_id, user2_id)
       DO NOTHING
-      RETURNING user1_id, user2_id, created_at
+      RETURNING id, user1_id, user2_id, created_at
     `;
     const res = await pool.query(query, [user1_id, user2_id]);
     return res.rows[0];
