2f9a52f766df8f95b71019b51fb6aa70d0ef957f
hosonu <hosonoyuki07@gmail.com>
2026-03-12 17:44:36 +0100

    chore: register reports tabale in init

--- backend/src/database/init.ts
+++ backend/src/database/init.ts
@@ -8,6 +8,7 @@ import { createBlockTable } from '../models/block.model.js';
 import { createConversationTable } from '../models/converstation.model.js';
 import { createMessageTable } from '../models/messages.model.js';
 import { createNotificationTable } from '../models/notification.model.js';
+import { createReportTable } from '../models/report.model.js';
 
 let pool: any;
 
@@ -41,6 +42,7 @@ export const initDB = async () => {
       await pool.query(createConversationTable);
       await pool.query(createMessageTable);
       await pool.query(createNotificationTable);
+      await pool.query(createReportTable);
       console.log('✅ Database initialized');
       return;
     } catch (err) {
