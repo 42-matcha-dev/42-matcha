68e2744addb57c2f53ea484d7a0595ce355a9175
odkaz <one.destiny.japan@gmail.com>
2026-03-19 22:52:48 +0100

    fix: build error data

--- frontend/src/app/schema.ts
+++ frontend/src/app/schema.ts
@@ -12,33 +12,28 @@ const longText = (min = 3, max = 150) =>
     .min(min, { message: `Text must contain at least ${min} characters.` })
     .max(max, { message: `Text must contain at most ${max} characters.` })
 
-const MIN_AGE = 18
-const MAX_AGE = 100
-
-const today = new Date()
-
-const minDate = new Date(
-  today.getFullYear() - MAX_AGE,
-  today.getMonth(),
-  today.getDate()
-)
-
-const maxDate = new Date(
-  today.getFullYear() - MIN_AGE,
-  today.getMonth(),
-  today.getDate()
-)
-
 export const registerSchema = z.object({
   email: z.email(),
   password: shortText(),
   repeatPassword: shortText(),
   firstName: shortText(),
   lastName: shortText(),
-  birthday: z.coerce
-    .date()
-    .min(minDate, "Age must be less than 100")
-    .max(maxDate, "You must be at least 18"),
+  birthday: z
+    .string()
+    .refine((val) => {
+      const date = new Date(val)
+      if (isNaN(date.getTime())) return false
+
+      const today = new Date()
+      let age = today.getFullYear() - date.getFullYear()
+      const m = today.getMonth() - date.getMonth()
+
+      if (m < 0 || (m === 0 && today.getDate() < date.getDate())) {
+        age--
+      }
+
+      return age >= 18 && age <= 100
+    }, "You must be between 18 and 100 years old"),
   location: z.string(),
   latitude: z.number(),
   longitude: z.number(),
