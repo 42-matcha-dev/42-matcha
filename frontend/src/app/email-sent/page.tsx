82aa5a80a742ca44f7c8fd883192845fec43e2be
hosonu <hosonoyuki07@gmail.com>
2026-03-19 20:08:56 +0100

    feat: add the button for back to login page on email-sent page

--- frontend/src/app/email-sent/page.tsx
+++ frontend/src/app/email-sent/page.tsx
@@ -1,13 +1,20 @@
 import Title from "@/app/components/Title";
+import Link from "next/link";
 
 export default function EmailSentPage() {
   return (
-    <div className="flex justify-center items-center min-h-screen w-1/2 h-full bg-white text-black p-4 border">
+    <div className="flex justify-center items-center min-h-screen h-full bg-white text-black p-4 border">
       <div className="flex flex-col items-center w-1/1.9 max-w-md p-5 gap-15">
         <Title
           title="Check Your Email"
           subTitle="We've sent a verification email. Please check your inbox to continue."
         />
+        <Link 
+          href="/login"
+          className="text-secondary font-semibold hover:underline"
+        >
+          Back to Login
+        </Link>
       </div>
     </div>
   );
