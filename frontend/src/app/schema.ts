e250ae787f219d21bafa3116521b12fdc8b6ea88
odkaz <one.destiny.japan@gmail.com>
2026-03-27 11:33:07 +0100

    update: changed error msg

--- frontend/src/app/schema.ts
+++ frontend/src/app/schema.ts
@@ -56,7 +56,7 @@ export const registerSchema = z.object({
     .array(z.number())
     .min(1, { message: 'Please select at least one tag.' })
     .max(5, { message: 'You can select up to 5 tags.' }),
-  iconUrl: z.string().min(1, "Please select an image").url(),
+  iconUrl: z.string().min(1, "Please select an image").url("Please select an image"),
   photoUrls: z
     .array(z.string().url().or(z.literal("")))
     .max(4)
